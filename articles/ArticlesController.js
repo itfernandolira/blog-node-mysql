956f7a4d7408cd89ba13363db30707facff9c18c
itfernandolira <it.fernandolira@gmail.com>
2021-03-16 09:56:38 +0000

    CRUD on Articles

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,8 +1,96 @@
 const express=require("express");
 const router=express.Router();
+const Category = require("../categories/Category");
+const slugify=require("slugify");
+const Article = require("./Article");
 
-router.get("/articles",(req,res)=>{
-    res.send("Articles route!");
+router.get("/admin/articles",(req,res)=>{
+    Article.findAll({
+        include: [{model: Category}],
+        order: [['title','ASC']]
+    }).then(articles => {
+            res.render("admin/articles/index",{
+                articles: articles
+            });
+        });
+});
+
+router.get("/admin/articles/new",(req,res)=>{
+    Category.findAll().then(categories => {
+        res.render("admin/articles/new",{categories: categories});
+    })
+});
+
+router.post("/articles/save",(req,res)=>{
+    var title=req.body.title;
+    var body=req.body.body;
+    var categoryId=req.body.category;
+    if (title!=undefined){
+        Article.create({
+            title: title,
+            body: body,
+            categoryId: categoryId,
+            slug: slugify(title,{
+                lower: true
+            })
+        }).then(()=>{
+            res.redirect("/admin/articles");
+        });
+    } else {
+        res.redirect("/admin/articles");
+    }
+});
+
+router.post("/articles/delete",(req,res)=>{
+    var id=req.body.id;
+    if (id!=undefined){
+        Article.destroy({
+            where : {
+                id:id
+            }
+        }).then(()=>{
+            res.redirect("/admin/articles");
+        });
+    } else {
+        res.redirect("/admin/articles");
+    }
+});
+
+router.get("/admin/articles/edit/:id",(req,res) => {
+    var id=req.params.id;
+    Article.findByPk(id).then(article => {
+        if (article!=undefined) {
+            Category.findAll().then(categories => {
+                res.render("admin/articles/edit",{article: article, categories: categories});
+            })
+        }else {
+            res.redirect("/admin/articles");
+        }
+    }).catch(error => {
+        res.redirect("/admin/articles");
+    });
+});
+
+router.post("/articles/update",(req,res)=> {
+    var id=req.body.id;
+    var title=req.body.title;
+    var body=req.body.body;
+    var categoryId=req.body.category;
+
+    Article.update({
+        title: title,
+        slug: slugify(title,{
+            lower: true
+        }),
+        body: body,
+        categoryId: categoryId
+    },{
+        where : {
+            id: id
+        }
+    }).then(()=>{
+        res.redirect("/admin/articles");
+    });
 });
 
 module.exports=router;
